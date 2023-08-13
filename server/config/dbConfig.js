0828b120dfcc188feac6a6880d04a5cb830f11c7
AbyssKnight7aken <7akenbg@gmail.com>
2023-08-13 12:20:07 +0300

    mongo string

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const dbName = 'travelr';
 
-const uri  = `mongodb://127.0.0.1:27017/${dbName}`;
+//const uri  = `mongodb://127.0.0.1:27017/${dbName}`;
+const uri  = `mongodb+srv://7aken:aLffW6wCs7HByLB@cluster0.otu3fel.mongodb.net/?retryWrites=true&w=majority`;
 
 async function dbConnect() {
     await mongoose.connect(uri);

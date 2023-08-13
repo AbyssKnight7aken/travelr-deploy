e6ccbd0e3805a00681bc12ab58fa3bf2b685aa53
AbyssKnight7aken <7akenbg@gmail.com>
2023-08-13 12:54:19 +0300

    server change

--- server/middlewares/corsMiddleware.js
+++ server/middlewares/corsMiddleware.js
@@ -1,7 +1,7 @@
 exports.useCORS = (req, res, next) => {
 
     //res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Access-Control-Allow-Origin", 'http://localhost:4200');
+    res.setHeader("Access-Control-Allow-Origin", 'http://localhost:4200', '*');
     res.setHeader("Access-Control-Allow-Credentials", true);
 
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');

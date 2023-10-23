e8bfda11807c41e2be90f59108dc9ea5a8704fc4
Radoslav Petrov <115632451+AbyssKnight7aken@users.noreply.github.com>
2023-10-24 02:36:40 +0300

    Update corsMiddleware.js

--- server/middlewares/corsMiddleware.js
+++ server/middlewares/corsMiddleware.js
@@ -1,7 +1,7 @@
 exports.useCORS = (req, res, next) => {
 
     //res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Access-Control-Allow-Origin", 'https://travelr-web-application.netlify.app/');
+    res.setHeader("Access-Control-Allow-Origin", 'https://travelr-web-application.netlify.app');
     res.setHeader("Access-Control-Allow-Credentials", true);
 
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
